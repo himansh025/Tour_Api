4281be340f6e6d88a89155b3e6ea60792b0387c7
himansh025 <himanshudeolia0825@gmail.com>
2024-12-17 13:05:00 +0530

    fixing cors

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 const express = require('express');
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const app = express();
 const path = require('path');
-const bodyParser = require("body-parser");
+const app = express();
 
-
-  const allowedOrigins = process.env.NODE_ENV === 'production' 
+// CORS configuration based on environment
+const allowedOrigins = process.env.NODE_ENV === 'production' 
   ? process.env.ALLOWED_ORIGINS?.split(',') || [] // Production URL(s)
   : ['http://localhost:5173']; // Local development URL
 
 console.log("Allowed Origins:", allowedOrigins);
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  }));
 
-  // app.options('*', cors());  // 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
+// Middleware for parsing incoming requests
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use('/public', express.static(path.join(__dirname, 'public')))
+
+// Static files configuration
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(bodyParser.json());
 
-
-
+// Importing Routes
 const bookingRoutes = require('./routes/bookingRoutes.js');
 const adminRoutes = require('./routes/adminRoutes.js');
 const userRoutes = require('./routes/userRoute.js');
 
-
+// Defining Routes
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
 app.use('/api/v1/admin', adminRoutes);
-
 
 module.exports = app;
